3ca3f10a075580fc56840d66988f4365c5679c46
Henriko Alberton <henriko.foz@gmail.com>
2022-09-11 21:09:29 -0300

    HOTFIX: CORS error with live environment

--- questao_7/src/server.ts
+++ questao_7/src/server.ts
@@ -1,9 +1,31 @@
-const express = require("express");
+import express, { Request, Response } from "express";
+import { NextFunction } from "express";
 import { CategoryRoutes } from "./routes/category.routes";
 
 const app = express();
 
 app.use(express.json());
+
+// Add headers
+app.use(function (req: Request, res: Response, next: NextFunction) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.use(CategoryRoutes);
 
